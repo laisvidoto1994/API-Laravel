7fa44075c5a4b0b36aa74c71bd6a3987993b23bb
laisvidoto1994 <lais.vidoto@gmail.com>
2018-04-03 13:07:26 -0300

    api-laravel, primeiro commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,98 @@
+<?php namespace App\Http\Controllers; 
+// namespace App\Http\Controllers-> rota onde está salva os controladores
+
+use App\User; 
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class UserController extends Controller 
+{  
+
+  public function showUser($id)
+  { 
+    // retorne um json do usuario com os dados do id informado, senão mostre erro por não achar-lo
+    return User::findOrFail($id); 
+  } 
+
+
+  public function cadastroUser(Request $request)
+  {
+    //mostra para o desenvolvedor como está sendo feita, e todos os registros do request.
+    \Log::debug($request->all());
+
+    $data = [
+              "name"     => $request->get('name'),//passando o parametro da request para name
+              "email"    => $request->get('email'),
+              "password" => bcrypt($request->get('password'))//bcrypt mostra senha criptografia
+            ];
+
+    try
+    {
+      $user = User::create($data); //criando usuario no banco de dados
+
+      if( empty($user) ) //se vazio
+      {
+        throw new \Exception('usuario vazio, não cadastrado');
+      }
+      return response()->json(['error' => false, 'message' => 'usuario criado com sucesso!'], 200); 
+    }
+    catch (\Exception $e)
+    {
+      // retorna um erro que foi passado no throw
+      return response()->json(['error' => true, 'message' => $e->getMessage()], 401);
+    }
+      
+  }
+
+
+  public function updateUser(Request $request)
+  {
+    //o que será/o que pode ser atualizado
+    $data = 
+    [
+      "name"     => $request->get('name'),//passando o parametro da request para name
+      "email"    => $request->get('email'),
+      "password" => bcrypt($request->get('password'))
+    ];
+
+    try
+    {
+      $user = User::where('id', $request->get('id') )->update($data);
+    
+      \Log::debug($user);
+
+      if( !$user )
+      {
+        throw new \Exception('usuario vazio, não atualizado');
+      }
+      return response()->json(['error' => false,'message' => 'usuario atualizado com sucesso!'], 200);
+    }
+    catch (\Exception $e)
+    {
+      return response()->json(['error' => true, 'message' => $e->getMessage()], 401);
+    }
+  }
+  
+  
+  public function deleteUser(Request $request)
+  {
+
+    try
+    {
+      $user = User::where('id', $request->get('id') )->delete();
+
+      if( !$user )
+      {
+        throw new \Exception('usuario inesistente, informe outro usuario!');
+      }
+      return response()->json(['error' => false,'message' => 'usuario excluido com sucesso!'], 200);
+    }
+    catch (\Exception $e)
+    {
+      return response()->json(['error' => true, 'message' => $e->getMessage()], 401);
+    }
+
+    }
+
+} 
---
+++ routes/api.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+| Aqui é onde registra as rotas da API para o aplicativo. 
+| Estas rotas são carregadas pelo RouteServiceProvider dentro de um grupo que
+| é atribuído o grupo de middleware "api".
+auth:api é o nome do arquivo
+*/
+ 
+Route::middleware('auth:api')->group(function()
+{
+    /** Rota 1
+    * http://127.0.0.1:8000/api/user
+    * get -> forma de identificar como quer realizar á comunicação(post,get,put)
+    * '/user'-> 
+    *  function -> direciona para o metodo o qual queria execultar algo
+    */
+    Route::get('/user', function (Request $request) 
+    {
+        return $request->user();
+    });
+ 
+    //post é para cadastro. e NÃO é passado via http. necessita de altenticação
+
+    /** Rota 2
+     * http://127.0.0.1:8000/api/user/1
+     * get -> forma de identificar como quer realizar á comunicação(post,get,put)
+     * 'user'-> 
+     * /{id}-> parametro de busca por id
+     *  UserController -> nome da classe controladora de metodos de requisição
+     * @showUser-> nome do metodo o qual estou me referindo
+     */
+    Route::get('user/{id}', 'UserController@showUser');
+
+    // rota de Cadastro dos dados no banco
+    Route::post('/user/create', 'UserController@cadastroUser');
+
+    // rota de Atualização dos dados do banco
+    Route::post('/user/update', 'UserController@updateUser');
+
+    // rota de Delete dos dados do banco
+    Route::post('/user/delete', 'UserController@deleteUser');  
+});
